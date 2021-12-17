f144f91d05aa14207a9530a73d31035dd2c365c8
GIJoeCodes <gijoecodes@gmail.com>
2021-12-16 23:06:50 -0500

    finish HomeController animation

---
+++ SwiftySideMenu/Extension/ReuseIdentifiable+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  ReuseIdentifiable+Ext.swift
+//  SwiftySideMenu
+//
+//  Created by GIJoeCodes on 12/16/21.
+//
+
+import Foundation
---
+++ SwiftySideMenu/Extension/UITableViewCell+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UITableViewCell+Ext.swift
+//  SwiftySideMenu
+//
+//  Created by GIJoeCodes on 12/16/21.
+//
+
+import Foundation
